133e307a3564d8e5d100ffdd9e324015c33bae00
Github d0ikim <doyclub@naver.com>
2026-05-14 16:10:13 +0900

    feat: Cucumber 테스트의존성 추가
    build.gradle.kts에
    cucumber-java,
    cucumber-spring,
    cucumber-junit-platform-engine,
    junit-platform-suite 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,12 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Cucumber
+	testImplementation("io.cucumber:cucumber-java:7.18.1")
+	testImplementation("io.cucumber:cucumber-spring:7.18.1")
+	testImplementation("io.cucumber:cucumber-junit-platform-engine:7.18.1")
+	testImplementation("org.junit.platform:junit-platform-suite")
 }
 
 tasks.withType<Test> {
